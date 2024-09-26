e77cf5adc723a54f21948a91d3c689ae86c3a1eb
Vida Zarei <vida1997zarei@gmail.com>
2024-09-26 10:52:03 -0400

    App component was updated

--- phonebook/src/App.tsx
+++ phonebook/src/App.tsx
@@ -1,8 +1,11 @@
-import React, { useReducer, FC } from "react";
+import React, { useReducer, FC, useState, useEffect } from "react";
 import Header from "./components/Header";
 import ContactForm from "./components/ContactForm";
 import { contactsReducer, AppState } from "./reducers/contactsReducer";
 import ContactList from "./components/ContactList";
+
+import { Contact } from "./types";
+import EditModal from "./components/EditModal";
 
 const initialState: AppState = {
   contacts: [],
@@ -10,12 +13,47 @@ const initialState: AppState = {
 
 const App: FC = () => {
   const [state, dispatch] = useReducer(contactsReducer, initialState);
+  const [showModal, setShowModal] = useState(false);
+  const [dataToEdit, setDataToEdit] = useState<Contact | undefined>(undefined);
+  useEffect(() => {
+    if (!showModal) {
+      setDataToEdit(undefined);
+    }
+  }, [showModal]);
+  const toggleModal = () => {
+    setShowModal((show) => !show);
+  };
+  const handleEdit = (id: number) => {
+    setDataToEdit(state.contacts.find((contact) => contact.id === id));
+    toggleModal();
+  };
+
   return (
     <>
       <Header />
-      <ContactForm dispatch={dispatch} />
-      {state.contacts.length > 0 && <ContactList contacts={state.contacts} />}
+      <div style={{ display: "flex" }}>
+        <ContactForm
+          dispatch={dispatch}
+          dataToEdit={dataToEdit}
+          toggleModal={toggleModal}
+        />
+        <hr />
+        {state.contacts.length > 0 && (
+          <ContactList
+            contacts={state.contacts}
+            handleEdit={handleEdit}
+            dispatch={dispatch}
+          />
+        )}
+      </div>
+      <EditModal
+        showModal={showModal}
+        dataToEdit={dataToEdit}
+        toggleModal={toggleModal}
+        dispatch={dispatch}
+      />
     </>
   );
 };
+
 export default App;
